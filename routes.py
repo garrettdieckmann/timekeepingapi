c2e4375e0fe0cc648d2cd90882cd3874afb782cc
Garrett Dieckmann <gdieckmann@gmail.com>
2013-04-17 03:55:10 +0000

    removed routes.py

--- routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, url_for
-app = Flask(__name__)
-
-@app.route('/')
-def api_root():
-	return 'Root of API'
-
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
